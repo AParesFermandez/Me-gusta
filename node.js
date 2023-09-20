753e8a5999163ea1af63b71cf9f0d4103b1d5a4c
AParAPares <amandapares87@gmail.com>
2023-09-20 03:22:46 -0300

    Part 2

--- node.js
+++ node.js
@@ -1,9 +1,13 @@
-// Inicializamos el contador en 9 (puedes establecer otro valor inicial si lo deseas)
-let likesCount = 9;
+// Crear el objeto likesCounts para almacenar la cantidad de likes por tarjeta
+const likesCounts = {
+    1: 9, // Inicializa con el valor inicial de la primera tarjeta
+    2: 12, // Inicializa con el valor inicial de la segunda tarjeta
+    3: 9, // Inicializa con el valor inicial de la tercera tarjeta
+};
 
 // Función para manejar el evento de clic en el botón "Like"
 function like(postId) {
-    likesCount++; // Aumentamos el contador de likes
+    likesCounts[postId]++; // Aumentamos el contador de likes
     const likesElement = document.getElementById(`post-${postId}`);
-    likesElement.textContent = `${likesCount} like(s)`; // Actualizamos el contenido del elemento span
+    likesElement.textContent = `${likesCounts[postId]} like(s)`; // Actualizamos el contenido del elemento span
 }
